3a24acda30808b385f7ece859bd3743e3c3449c7
kvodengy <149088432+kvodengy@users.noreply.github.com>
2024-03-07 18:35:11 +0200

    Add files via upload

---
+++ labyrinth.py
@@ -0,0 +1,89 @@
+import pygame
+
+FPS = pygame.time.Clock()
+win_width = 1280
+win_height = 665
+p_size = 100
+
+
+window = pygame.display.set_mode((win_width, win_height))
+pygame.display.set_caption("Лабіринт")
+
+class Settings():
+    def __init__(self, image, x, y, w, h):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.image = pygame.image.load(image)
+        self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))
+
+    def draw(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(Settings):
+    def __init__(self, image, x, y, w, h, s):
+        super().__init__(image, x, y, w, h)
+        self.speed = s
+        self.flipped_image = pygame.transform.flip(self.image, True, False)
+        self.no_flip_image = self.image
+        self.flip = True
+
+    def move(self):
+        if self.flip:
+            self.image = self.no_flip_image
+        else:
+            self.image = self.flipped_image
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP] and self.rect.y>0:
+            self.rect.y -= self.speed
+        if keys[pygame.K_DOWN] and self.rect.y<win_height-self.rect.height:
+            self.rect.y += self.speed
+        if keys[pygame.K_LEFT] and self.rect.x>0:
+            self.rect.x -= self.speed
+            self.flip = False
+        if keys[pygame.K_RIGHT] and self.rect.x<win_width-self.rect.width:
+            self.rect.x += self.speed 
+            self.flip = True
+
+class Enemy(Player):
+    def __init__(self, image, x, y, w, h, s):
+        super().__init__(image, x, y, w, h, s)
+        self.direction = True #right
+
+    def move(self, x1, x2):
+        if self.rect.x<x1:
+            self.direction = True
+        elif self.rect.x>x2:
+            self.direction = False
+
+        if self.direction:
+            self.image = self.no_flip_image
+            self.rect.x += self.speed
+        else:
+            self.image = self.flipped_image
+            self.rect.x -= self.speed
+        
+        
+
+
+bg = Settings("background.png", 0, 0, win_width, win_height)
+p1 = Player("sprite1.png", 0, win_height//2, p_size, p_size, 5)
+enemy = Enemy("sprite2.png", win_width//1.3, win_height//2.3, p_size*1.5, p_size*1.5, 2)
+gold = Settings("gold.png", win_width//1.15, win_height//1.5, p_size, p_size)
+game = True
+
+pygame.mixer.init()
+pygame.mixer.music.load("music.mp3")
+pygame.mixer.music.play()
+
+while game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game = False
+
+    bg.draw()
+    p1.move()
+    p1.draw()
+    enemy.move(win_width//1.3, win_width//1.12)
+    enemy.draw()
+    gold.draw()
+    pygame.display.flip()
+    FPS.tick(40)
