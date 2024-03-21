39a40e0fb32bdfd3959f5037ca25daccdbaae9dc
kvodengy <149088432+kvodengy@users.noreply.github.com>
2024-03-21 17:42:36 +0200

    Add files via upload

--- labyrinth.py
+++ labyrinth.py
@@ -1,4 +1,5 @@
 import pygame
+pygame.font.init()
 
 FPS = pygame.time.Clock()
 win_width = 1280
@@ -74,6 +75,32 @@ class Wall:
         self.color = color
     def draw(self):
         pygame.draw.rect(window ,self.color, self.rect)
+
+class Area():
+    def __init__(self,x=0,y=0, width = 10, height = 10, color = None):
+        self.rect = pygame.Rect(x,y,width,height)
+        self.fill_color = color
+
+    def color(self, new_color):
+        self.fill_color = new_color
+
+    def fill(self):
+        pygame.draw.rect(window, self.fill_color, self.rect)
+
+    def outline(self, frame_color, thickness):
+        pygame.draw.rect(window, frame_color, self.rect, thickness)
+
+    def collidepoint(self,x,y):
+        return self.rect.collidepoint(x,y)
+
+class Label(Area):
+    def __init__(self,size,color):
+        self.size = size
+        self.color = color
+    def set_text(self, text):
+        self.text = pygame.font.SysFont('verdana', self.size).render(text, True, self.color)
+    def draw(self, x, y):
+        window.blit(self.text, (x,y))
         
         
         
@@ -81,8 +108,11 @@ class Wall:
 
 bg = Settings("background.png", 0, 0, win_width, win_height)
 p1 = Player("sprite1.png", 0, win_height//2, p_size, p_size, 3)
-enemy = Enemy("sprite2.png", win_width//1.3, win_height//1.7, p_size*3, p_size*3, 4)
+enemy = Enemy("sprite2.png", win_width//1.3, win_height//1.7, p_size*2, p_size*2, 3)
 gold = Settings("gold.png", win_width//1.15, win_height//1.2, p_size*1.5, p_size*1.5)
+win = Label(100,(0,255,0))
+win.set_text("You won!")
+
 walls = [
     Wall(win_width//4,win_height//8, win_width//2, 5, (0,200,255)),
     Wall(win_width//4,win_height//1.1, win_width//2, 5, (0,200,255)),
@@ -107,24 +137,36 @@ walls = [
 ]    
 
 game = True
+finish = False
 
 pygame.mixer.init()
 pygame.mixer.music.load("music.mp3")
+lose = pygame.mixer.Sound("lose.wav")
 pygame.mixer.music.play()
 
-while game:
+
+while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-    bg.draw()
-    p1.move()
-    p1.draw()
-    for w in walls:
-        w.draw()
-        if p1.rect.colliderect(w.rect):
-            p1 = Player("sprite1.png", 0, win_height//2, p_size, p_size, 3)
-    enemy.move(win_width//1.3, win_width//1.12)
-    enemy.draw()
-    gold.draw()
+
+    if finish != True:
+        bg.draw()
+        p1.move()
+        p1.draw()
+        
+        for w in walls:
+            w.draw()
+            if p1.rect.colliderect(w.rect) or p1.rect.colliderect(enemy.rect):
+                p1 = Player("sprite1.png", 0, win_height//2, p_size, p_size, 3)
+                lose.play()
+
+        if p1.rect.colliderect(gold.rect):
+            win.draw(win_width//3, win_height//3)
+            pygame.mixer.music.stop()
+            finish = True
+        enemy.move(win_width//1.3, win_width//1.12)
+        enemy.draw()
+        gold.draw()
     pygame.display.flip()
     FPS.tick(40)
